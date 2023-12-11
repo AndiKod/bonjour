7b8bac276929ba81b5cbf8ead4edfc3042efefa3
AndiKod <andrei@andikod.fr>
2023-12-11 15:20:10 +0100

    Added a live clock

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 // Drop your JS here ;)
 
-console.log('Hey there!')
+// The Clock
+const showTime = async () => { while (true) {
+
+  const elClock = document.querySelector('#clock');
+  const d = new Date();
+  const h = d.getHours();
+  const m = String(d.getMinutes()).padStart(2, '0');
+
+  elClock.innerHTML = `${h}:${m}`;
+
+  await new Promise(resolve => setTimeout(resolve, 1000));
+}};
+showTime();
